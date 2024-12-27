8c508478589831f7b9a881f2de0c39c7c1d4b0fe
ilhamakbarjamil <ilhamakbarjamil8@gmail.com>
2024-12-28 04:20:24 +0700

    akbar

--- gaji/src/Main.java
+++ gaji/src/Main.java
@@ -1,8 +1,16 @@
-public class Main {
-    class Employee {
-        String nama;
-        int umur;
-        double gaji;
-        String posisi;
+
+class Employee {
+    String nama;
+    int umur;
+    double gaji;
+    String posisi;
+
+    public Employee(String nama, int umur, double gaji, String posisi) {
+        this.nama = nama;
+        this.umur = umur;
+        this.gaji = gaji;
+        this.posisi = posisi;
     }
 }
+
+
